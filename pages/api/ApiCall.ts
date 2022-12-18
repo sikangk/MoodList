4643cf3b5da1a5ef5b469b4cb0bff68a0b839ac0
sikangk <updream4@naver.com>
2022-12-18 23:45:39 +0900

    feat: add yotube background container

--- pages/api/ApiCall.ts
+++ pages/api/ApiCall.ts
@@ -4,9 +4,7 @@ import axios, { AxiosResponse } from 'axios';
 const ApiCall = async (url: string) => {
     const api: AxiosResponse<any> = await axios.get(url);
 
-
-    console.log(api, 'api');
-
+    return api;
 }
 
 export default ApiCall;
--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,66 +1,104 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import APIURL from '../api/APIURL';
 import ApiCall from '../api/ApiCall';
 import Youtube from 'react-youtube';
 
 const Container = styled.div`
-    width:100%;
-    height:100vh;
-    display:flex;
-    justify-content:center;
-    align-items:center
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+
 
 `;
 
 const MoodWrap = styled.div`
-    width:80%;
-    height:50vh;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    flex-wrap:wrap;
+  width: 80%;
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
 `;
 
 const MoodBox = styled.div`
-    
-    width:20%;
-    height:25vh;
-    border:1px solid black;
-    display:flex;
-    justify-content:center;
-    align-items:center
-  
+
+  width: 20%;
+  height: 25vh;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
 `;
 
 
 const moodArr = [
-    { id: 1, mood: '행복' },
-    { id: 2, mood: '슬픔' },
-    { id: 3, mood: '짜증' },
-    { id: 4, mood: '분노' },
-    { id: 5, mood: '우울' },
-    { id: 6, mood: '신남' },
-    { id: 7, mood: '질투' },
-    { id: 8, mood: '창피' }
+    {id: 1, mood: '행복' , value:'행복한 음악'},
+    {id: 2, mood: '슬픔', value:'슬픈 음악'},
+    {id: 3, mood: '짜증'},
+    {id: 4, mood: '분노'},
+    {id: 5, mood: '우울'},
+    {id: 6, mood: '신남'},
+    {id: 7, mood: '질투'},
+    {id: 8, mood: '창피'}
 ]
 
-const getList = () => {
-
-    ApiCall(APIURL.search + `?part=snippet&q=즐거운음악&order=viewCount&key=AIzaSyCZtze9Mpjl7KnNA7RiXJYmg5LJVBu5nqo`)
-}
 
 // console.log(APIURL.search + `?part=snippet&q=즐거운음악&key=AIzaSyCZtze9Mpjl7KnNA7RiXJYmg5LJVBu5nqo`)
 
 
 function Main() {
-    return (
-        <>
-            {/* <div style={{ position: 'fixed', zIndex: -99, width: '100%', height: '100vh' }}>
+    const [playList, setPlayList] = useState([]);
+    const [globalHeight,setGlobalHeight] = useState(0);
 
-          
-                <Youtube videoId='HLxo8Gr8qRA' opts={{ height: '100vh', width: '100%', playerVars: { autoplay: 1, } }} />
-            </div> */}
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            setGlobalHeight(window.innerHeight);
+
+        },1000)
+
+
+    },[])
+
+    const getList = async () => {
+
+
+        try {
+            const response = await ApiCall(APIURL.search + `?part=snippet&q=즐거운음악&order=viewCount&key=AIzaSyCZtze9Mpjl7KnNA7RiXJYmg5LJVBu5nqo`);
+
+            const {data} = response;
+
+            const {items} = data;
+
+            console.log(items,'data');
+            setPlayList(data);
+
+
+
+            console.log(response, 'response');
+
+            console.log(playList, 'playList');
+
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+
+    return (
+        <div style={{width: '100%', height: '100vh', position: 'relative',display:'flex',justifyContent:'center',alignItems:'center'}}>
+            <div style={{position: 'absolute', zIndex: -99, width: '100%', height: '100vh', border: '1px solid black'}}>
+
+
+                <Youtube videoId='HLxo8Gr8qRA'
+                         style={{}} opts={{width: '100%', playerVars: {autoplay: 1,}}}/>
+            </div>
             <Container>
 
                 <MoodWrap>
@@ -73,7 +111,7 @@ function Main() {
                     })}
                 </MoodWrap>
             </Container>
-        </>
+        </div>
     )
 }
 
